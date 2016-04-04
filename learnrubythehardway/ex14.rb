704a9652879e7861603b1c454b23e9383e804112
Hoàng Phi <lokialicehd@gmail.com>
2016-04-04 14:53:57 +0700

    Prompting and Passing

---
+++ learnrubythehardway/ex14.rb
@@ -0,0 +1,22 @@
+user_name = ARGV.first #gets the first argument
+promt = '>'
+
+puts "Hi #{user_name}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}?"
+puts promt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{user_name} ?"
+puts promt
+lives = $stdin.gets.chomp
+
+#a comma for puts is like using it twice
+puts "What kind of computer do you have?", promt
+computer = $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}. Not sure where that is.
+And you have a #{computer} computer. Nice.
+"""
